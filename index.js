6107b7a5b0b54f35308a951a9fb484de527efea6
Seunggyu <seunggyul14@icloud.com>
2022-11-29 20:17:23 -0800

    commit tree 1d0ff8b0e0fc912e91131a3e5cb189323dd47ad6

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+   .then(response => response.json())
+   .then((json) => console.log(json));
 }
+
+
+
+
+
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +25,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+  
